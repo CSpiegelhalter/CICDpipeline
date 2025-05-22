121ab86cda7cd61342b4a7196439fae9921e4a91
Curt Spiegelhalter <spiegelhaltercurt@gmail.com>
2025-05-22 10:27:26 -0700

    All files

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Devloping locally
+
+1. Install `kind`
+`brew install kind`
+
+2. Create local k8 cluster
+`kind create cluster --name todo-dev`
---
+++ todo-api/main.py
@@ -0,0 +1,16 @@
+# main.py
+from fastapi import FastAPI
+from prometheus_client import Counter, generate_latest
+from starlette.responses import Response
+
+app = FastAPI()
+hits = Counter("hits", "Number of hits to the root")
+
+@app.get("/")
+def read_root():
+    hits.inc()
+    return {"message": "Hello World"}
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type="text/plain")
